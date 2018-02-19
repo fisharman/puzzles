366c9b030fbd56eb5b964305962d56f516df9255
Ray Zhou <zhou.ray@gmail.com>
2018-02-20 01:28:58 +0800

    adding new files

---
+++ ArrayBST/Solution.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String args[] ) throws Exception {
+        Scanner in = new Scanner(System.in);
+        int q = in.nextInt();
+        for (int i = 0; i < q; i++){
+            int n = in.nextInt();
+
+            ArrayList<Integer> tree = new ArrayList<>();
+            for (int j = 0; j < n; j++){
+                 tree.add(in.nextInt());
+            }
+
+            // do processing here
+
+
+        }
+
+
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    }
+}
